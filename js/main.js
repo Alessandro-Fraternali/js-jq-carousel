4725ced1c770fda8e549635ad9d9ad1550cde431
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-02 18:27:08 +0200

    Inseriti i commenti guida

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// DESCRIZIONE:
+// ricreo lo slider come fatto questa mattina
+// faccio funzionare il tracciamento dei pallini sia next che prev
+// creo la possibilità di poter navigare cliccando anche sui pallini
+// cerco di fare un refactoring del mio codice
+
+// Creo due eventi di callback da richiamare per
+// quando l'utente clicca freccia avanti/indietro
+
+// Funzione per la freccia avanti
+
+
+// Funzione per la freccia indietro
