b770ec6b7a087dbe988db01befec1c2a5ec05888
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-02 20:31:13 +0200

    finita freccia destra

--- js/main.js
+++ js/main.js
@@ -3,11 +3,42 @@
 // faccio funzionare il tracciamento dei pallini sia next che prev
 // creo la possibilità di poter navigare cliccando anche sui pallini
 // cerco di fare un refactoring del mio codice
+$(document).ready(function(){
 
 // Creo due eventi di callback da richiamare per
 // quando l'utente clicca freccia avanti/indietro
+$(".next").click(
+  right
+);
+
+// $(".prev").click(
+//   left
+// );
 
 // Funzione per la freccia avanti
+function right() {
 
+  // riferimento dell'immagine a schermo
+  var onscreen = $('.images img.active');
+
+  // pallino attivo
+  var diglett = $(".nav i.active");
+
+  // rimuovo la classe active a img e pallino
+  onscreen.removeClass("active");
+  diglett.removeClass("active");
+
+  // verifico se l'immagine onscreen è l'ultima
+  if(onscreen.hasClass("last")){
+    $("images img.first").addClass("active");
+    $('.nav i.first').addClass('active');
+  } else {
+  // applico .active all'immagine seguente
+  onscreen.next().addClass("active");
+  diglett.next().addClass("active");
+  }
+}
 
 // Funzione per la freccia indietro
+
+});
